8e61d294b3c0906ce7d20462c700dd148b11df62
Fabio Salvalai <fabio@seesharp.ch>
2024-06-09 16:10:34 +0200

    caching validated IDs

--- src/services/playlistService.ts
+++ src/services/playlistService.ts
@@ -2,6 +2,7 @@ import { getTrackDetails, Track, getRecentlyPlayedTracks } from './spotifyServic
 import { getSuggestions, searchTrack, SuggestionRequest } from './deejaiService';
 
 const BATCH_SIZE = 40 ;
+const LOCAL_STORAGE_KEY = 'validatedTrackIds';
 
 export type ProgressCallback = (progress: { phase: string; percentage: number }) => void;
 
@@ -25,62 +26,74 @@ const searchForTrackId = async (track: Track): Promise<string | null> => {
  * @returns An alternative playlist.
  */
 export const buildAlternativePlaylist = async (
-  token: string,
-  tracks: Track[],
-  lengthMultiplier = 1,
-  progressCallback: ProgressCallback
-): Promise<Track[]> => {
-  progressCallback({ phase: 'Fetching recently played tracks', percentage: 10 });
-  const recentlyPlayed = await getRecentlyPlayedTracks(token);
-  const recentlyPlayedUris = new Set(recentlyPlayed.items.map(item => item.track.uri));
-  const existingUris = new Set(tracks.map(track => track.uri));
-
-  const validTrackIds: string[] = [];
-  for (let i = 0; i < tracks.length; i++) {
-    const track = tracks[i];
-    progressCallback({ phase: `Searching for track ID (${i + 1}/${tracks.length})`, percentage: 20 + (i / tracks.length) * 30 });
-    try {
-      const validTrackId = await searchForTrackId(track);
-      if (validTrackId) {
-        validTrackIds.push(validTrackId);
+    token: string,
+    tracks: Track[],
+    lengthMultiplier = 1,
+    progressCallback: ProgressCallback
+  ): Promise<Track[]> => {
+    const validatedTrackIds = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+  
+    progressCallback({ phase: 'Fetching recently played tracks', percentage: 10 });
+    const recentlyPlayed = await getRecentlyPlayedTracks(token);
+    const recentlyPlayedUris = new Set(recentlyPlayed.items.map(item => item.track.uri));
+    const existingUris = new Set(tracks.map(track => track.uri));
+  
+    const validTrackIds: string[] = [];
+    for (let i = 0; i < tracks.length; i++) {
+      const track = tracks[i];
+      progressCallback({ phase: `Searching for track ID (${i + 1}/${tracks.length})`, percentage: 20 + (i / tracks.length) * 30 });
+      const cachedTrackId = validatedTrackIds[track.uri];
+  
+      if (cachedTrackId) {
+        validTrackIds.push(cachedTrackId);
+      } else {
+        try {
+          const validTrackId = await searchForTrackId(track);
+          if (validTrackId) {
+            validTrackIds.push(validTrackId);
+            validatedTrackIds[track.uri] = validTrackId;
+          }
+        } catch (error) {
+          console.error('Error searching for track ID:', error);
+        }
       }
-    } catch (error) {
-      console.error('Error searching for track ID:', error);
     }
-  }
+  
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(validatedTrackIds));
+  
     validTrackIds.forEach((trackId) => {
-        existingUris.add(`spotify:track:${trackId}`);
+      existingUris.add(`spotify:track:${trackId}`);
     });
-
-  const alternativePlaylist: Track[] = [];
-  const adjustedBatchSize = Math.round(BATCH_SIZE / lengthMultiplier);
-  for (let i = 0; i < validTrackIds.length; i += adjustedBatchSize) {
-    const batch = validTrackIds.slice(i, i + adjustedBatchSize);
-    const payload: SuggestionRequest = {
-      track_ids: batch,
-      size: lengthMultiplier,
-      creativity: 0.99,
-      noise: 0,
-    };
-
-    progressCallback({ phase: `Fetching suggestions for batch ${i / adjustedBatchSize + 1}`, percentage: 50 + (i / validTrackIds.length) * 50 });
-
-    try {
-      const suggestionResponse = await getSuggestions(payload);
-      const suggestionTracks = await Promise.all(
-        suggestionResponse.track_ids.map(trackId => getTrackDetails(trackId, token))
-      );
-
-      const filteredTracks = suggestionTracks.filter(
-        (track: Track) => !recentlyPlayedUris.has(track.uri) && !existingUris.has(track.uri)
-      );
-
-      alternativePlaylist.push(...filteredTracks);
-    } catch (error) {
-      console.error('Error fetching suggestions:', error);
+  
+    const alternativePlaylist: Track[] = [];
+    const adjustedBatchSize = Math.round(BATCH_SIZE / lengthMultiplier);
+    for (let i = 0; i < validTrackIds.length; i += adjustedBatchSize) {
+      const batch = validTrackIds.slice(i, i + adjustedBatchSize);
+      const payload: SuggestionRequest = {
+        track_ids: batch,
+        size: lengthMultiplier,
+        creativity: 0.99,
+        noise: 0,
+      };
+  
+      progressCallback({ phase: `Fetching suggestions for batch ${i / adjustedBatchSize + 1}`, percentage: 50 + (i / validTrackIds.length) * 50 });
+  
+      try {
+        const suggestionResponse = await getSuggestions(payload);
+        const suggestionTracks = await Promise.all(
+          suggestionResponse.track_ids.map(trackId => getTrackDetails(trackId, token))
+        );
+  
+        const filteredTracks = suggestionTracks.filter(
+          (track: Track) => !recentlyPlayedUris.has(track.uri) && !existingUris.has(track.uri)
+        );
+  
+        alternativePlaylist.push(...filteredTracks);
+      } catch (error) {
+        console.error('Error fetching suggestions:', error);
+      }
     }
-  }
-
-  progressCallback({ phase: 'Completed', percentage: 100 });
-  return alternativePlaylist;
-};
+  
+    progressCallback({ phase: 'Completed', percentage: 100 });
+    return alternativePlaylist;
+  };
